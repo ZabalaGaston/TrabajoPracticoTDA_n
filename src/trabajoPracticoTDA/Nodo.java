3abae25e30652a5c118b8a94738ba2d2041fd069
ZabalaGaston <49344960+ZabalaGaston@users.noreply.github.com>
2019-05-12 15:02:35 -0300

    Add files via upload

---
+++ src/trabajoPracticoTDA/Nodo.java
@@ -0,0 +1,23 @@
+package trabajoPracticoTDA;
+
+public class Nodo {
+
+	private Object valor;
+    private Nodo siguiente;
+
+   public Object getValor() {
+		return valor;
+	}
+
+	public void setValor(Object valor) {
+		this.valor = valor;
+	}
+
+	public Nodo getSiguiente() {
+		return siguiente;
+	}
+
+	public void setSiguiente(Nodo siguiente) {
+        this.siguiente = siguiente;
+    }   
+}
